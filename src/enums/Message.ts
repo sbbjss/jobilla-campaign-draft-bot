527de42a57d020fb50ecd6c2b03c522820563a86
sbbjss <agnomad13@gmail.com>
2023-09-04 15:57:08 +0200

    rebase

--- src/enums/Message.ts
+++ src/enums/Message.ts
@@ -4,6 +4,10 @@ export enum Message {
         'All sensitive data about customer and recruitment are not shared with OpenAI and not stored anywhere.',
     ChoosePII =
         'There are two ways to start: \n' +
-        '• Make sure your campaign draft has no sensitive data and send it to me. \n' +
+        '• Make sure your campaign draft has no sensitive data and send it to Jobilla Bot. \n' +
         '• Leave your campaign draft as it is and let Jobilla Bot handle it.',
+    SendCampaignDraft = 'Please send a message with your campaign draft, or a .docx file containing it.',
+    IncorrectFile = 'The file is not a valid .docx file. Start over?',
+    SomethingWentWrong = 'Something went wrong. Please try again.',
+    FileSuccess = 'Success! Processing your campaign draft...',
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
-import { Telegraf, Markup } from 'telegraf';
-import { message } from "telegraf/filters";
+import axios from 'axios';
+import { createDocumentParser } from '@arbs.io/asset-extractor-wasm';
+import { Context, Markup, Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 import { Message } from './enums/Message';
 import { ButtonLabel } from './enums/ButtonLabel';
 
@@ -21,45 +23,104 @@ const openAI = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const sendPromptToOpenAI = async (ctx: Context, userInput: string) => {
+    ctx.sendChatAction('typing');
+    const response = await openAI.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: userInput }],
+        max_tokens: 2000,
+        temperature: 0.5,
+        stream: false,
+    });
+
+    return response.choices[0].message?.content as string;
+};
+
+// Greets user
 bot.start((ctx) => ctx.reply(
     Message.Welcome,
     Markup.keyboard([ButtonLabel.GetStarted]).resize(),
 ));
 
+// User selects way to handle PII
 bot.hears(ButtonLabel.GetStarted, (ctx) => {
     ctx.reply(
         Message.ChoosePII,
         Markup.keyboard([ButtonLabel.NoPIISent, ButtonLabel.PIIConsent]).resize(),
-    )
+    );
 });
 
-bot.on(message('text'),  async (ctx) => {
-    const message = ctx.update.message.text;
-    messagesHistory.push({ role: 'user', content: message });
-    declutterMessagesHistory();
+// User opts out of sending PII and uploads campaign draft
+bot.hears(ButtonLabel.NoPIISent, async (ctx) => {
+    ctx.reply(Message.SendCampaignDraft, Markup.removeKeyboard());
+});
 
-    console.log(messagesHistory);
+// On sending campaign draft as text
+bot.on(message('text'), async ctx => {
+    const userInput = ctx.update.message.text;
+    messagesHistory.push({ role: 'user', content: userInput });
 
     try {
-        ctx.sendChatAction('typing');
-        const response = await openAI.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: messagesHistory,
-          max_tokens: 2000,
-          temperature: 0.5,
-          stream: false,
-        });
-    
-        messagesHistory.push(response.choices[0].message);
+        const gptResponse = await sendPromptToOpenAI(ctx, userInput);
+        messagesHistory.push(gptResponse);
         declutterMessagesHistory();
-        ctx.reply(response.choices[0].message?.content as string);
-      } catch (err) {
+        ctx.reply(gptResponse);
+    } catch ( err ) {
         console.log('ChatGPT error: ' + err);
-      }
+    }
+});
+
+// On sending campaign draft as docx
+bot.on(message('document'), async ctx => {
+    const mimeType = ctx.message.document.mime_type;
+
+    // Check if the uploaded document is of type txt, docx, or pdf
+    if (!mimeType || mimeType !== 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+        ctx.reply(
+            Message.IncorrectFile,
+            Markup.keyboard([ButtonLabel.GetStarted]).resize(),
+        );
+
+        return;
+    }
+
+    ctx.reply(Message.FileSuccess, Markup.removeKeyboard());
+
+    const fileLink = await ctx.telegram.getFileLink(ctx.message.document.file_id);
+    const response = await axios.get(fileLink.toString());
+
+    if (!response?.data) {
+        ctx.reply(
+            Message.SomethingWentWrong,
+            Markup.keyboard([ButtonLabel.GetStarted]).resize(),
+        );
+
+        return;
+    }
+
+    const userInput = createDocumentParser(new Uint8Array(response.data))?.contents?.text;
+
+    if (userInput == null) {
+        ctx.reply(
+            Message.SomethingWentWrong,
+            Markup.keyboard([ButtonLabel.GetStarted]).resize(),
+        );
+
+        return;
+    }
+
+    messagesHistory.push({ role: 'user', content: userInput });
+
+    try {
+        const gptResponse = await sendPromptToOpenAI(ctx, userInput);
+        ctx.reply(gptResponse);
+    } catch ( err ) {
+        console.log('ChatGPT error: ' + err);
+    }
 });
 
 bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
