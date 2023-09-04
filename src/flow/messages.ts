a9cce962d0d667f57066b0cb33af706682c6e1be
sbbjss <agnomad13@gmail.com>
2023-09-04 12:40:58 +0200

    restructure refactor

---
+++ src/flow/messages.ts
@@ -0,0 +1,4 @@
+export enum Message {
+    Welcome = 'Welcome dear Jobillian! This tool is created to help you create a campaign draft easily. All sensitive data about customer and recruitment are not shared with OpenAI and not stored anywhere.',
+    Help = 'Send me a sticker',
+}
