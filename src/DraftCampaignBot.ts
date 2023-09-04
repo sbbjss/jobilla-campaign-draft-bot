61b853809b06964695d8850147cce1b11f3f9e17
Zhenya Popov <zhenya.popov@jobilla.com>
2023-09-04 11:44:34 +0200

    answers with your message

--- src/DraftCampaignBot.ts
+++ src/DraftCampaignBot.ts
@@ -12,6 +12,13 @@ const openAI = new OpenAI({
 
 bot.start((ctx) => ctx.reply('Welcome dear Jobillian! This tool is created to help you create a campaign draft easily. All sensitive data about customer and recruitment are not shared with OpenAI and not stored anywhere'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('text'), (ctx) => ctx.reply('I\'m still in the works!'));
+bot.on(message('text'),  (ctx) => {
+    ctx.reply('Now some magic will happen, pls wait!');
+    const message = ctx.update.message.text;
+    ctx.reply('Your message: ' + message);
+});
 bot.launch();
 
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
