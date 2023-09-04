74266ce38dee8da2983ab59408f9098e41474ff8
sbbjss <agnomad13@gmail.com>
2023-09-04 10:55:15 +0200

    lint

--- main.js
+++ main.js
@@ -10,11 +10,7 @@ const openAI = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-// bot.start('start', (ctx) => {
-//     ctx.reply('Welcome dear Jobillian! This tool is created to help you create a campaign draft easily.');
-//     ctx.reply('All sensitive data about customer and recruitment are not shared with OpenAI and not stored anywhere');
-// });
-
 bot.start((ctx) => ctx.reply('Welcome dear Jobillian! This tool is created to help you create a campaign draft easily. All sensitive data about customer and recruitment are not shared with OpenAI and not stored anywhere'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on(message('text'), (ctx) => ctx.reply('I\'m still in the works!'));
+
